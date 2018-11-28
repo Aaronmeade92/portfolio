d890f3fd9e127c446d5785e70a6c5037956924cc
Aaron <aaronmeade9292@gmail.com>
2018-11-27 17:27:55 -0800

    add a side nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 
 const navDiv = {
     height: '100%',
-    width: '0',
+    width: '10%',
     position: 'fixed',
     zIndex: '1',
     top: '0',
@@ -16,15 +16,20 @@ const navDiv = {
 }
 const textStyle = {
     color: 'black',
-    display: 'block',
+    display: 'flex',
     transition: '0.3s',
     padding: '5px',
     paddingLeft: '8px',
-    textDecoration: 'none'
+    textDecoration: 'none',
+    justifyContent: 'flexStart',
+    
 }
 
 const hiddenStyle = {
     color: 'blue',
+    fontSize: '.7em',
+    display: 'flex',
+    paddingLeft: '.5em',
 }
 
 class NavBar extends Component {
@@ -32,19 +37,34 @@ class NavBar extends Component {
     state = {
         showProjects: false,
         showAbout: false,
+        showGithub: false,
+        showLinkedin: false,
     }
-
+    // REFACTOR
     projectInfo = () => {
         this.setState({ showProjects: true });
     }
     aboutInfo = () => {
         this.setState({ showAbout: true });
     }
+    githubInfo = () => {
+        this.setState({ showGithub: true });
+    }
+    linkedinInfo = () => {
+        this.setState({ showLinkedin: true });
+    }
+    // REFACTOR
     hideProject = () => {
         this.setState({ showProjects: false });
     }
     hideAbout = () => {
         this.setState({ showAbout: false });
+    }
+    hideGithub = () => {
+        this.setState({ showGithub: false });
+    }
+    hideLinkedin = () => {
+        this.setState({ showLinkedin: false });
     }
 
     render() {
@@ -52,10 +72,10 @@ class NavBar extends Component {
             <div style={navDiv}>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
-                <NavLink style={textStyle} onMouseEnter={this.projectInfo} onMouseLeave={this.hideProject} class='fa fa-code'to='/projects'>{this.state.showProjects && <p style={hiddenStyle} >INFO</p> }</NavLink>
-                <NavLink style={textStyle} onMouseEnter={this.aboutInfo} onMouseLeave={this.hideAbout} class='fa fa-search' to='/about'>{this.state.showAbout && <p style={hiddenStyle} >INFO</p> }</NavLink>
-                <li><a style={textStyle} href="https://github.com/aaronmeade92" class="fa fa-github"></a></li>
-                <li><a style={textStyle} href="https://www.linkedin.com/in/aaron-meade/" class="fa fa-linkedin"></a></li>
+                <NavLink style={textStyle} onMouseEnter={this.projectInfo} onMouseLeave={this.hideProject} class='fa fa-code'to='/projects'>{this.state.showProjects && <p style={hiddenStyle} >My Projects</p> }</NavLink>
+                <NavLink style={textStyle} onMouseEnter={this.aboutInfo} onMouseLeave={this.hideAbout} class='fa fa-search' to='/about'>{this.state.showAbout && <p style={hiddenStyle} >About Me</p> }</NavLink>
+                <li><a style={textStyle} onMouseEnter={this.githubInfo} onMouseLeave={this.hideGithub} href="https://github.com/aaronmeade92" class="fa fa-github">{this.state.showGithub && <p style={hiddenStyle} >My code</p> }</a></li>
+                <li><a style={textStyle} onMouseEnter={this.linkedinInfo} onMouseLeave={this.hideLinkedin} href="https://www.linkedin.com/in/aaron-meade/" class="fa fa-linkedin">{this.state.showLinkedin && <p style={hiddenStyle} >My LinkedIn</p> }</a></li>
 
             </div>
         );
